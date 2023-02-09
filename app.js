625421d61af7838c55ff76f054aa96fe84e0796a
shayan khan333 <shayan.13579.khan@gmail.com>
2023-02-10 00:54:16 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var a =  15;
+var value1 = a++ + ++a  -  --a;
+         //  =15+17-16
+         //  =16
+         console.log(value1)
+ var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
+        // =16+17-16+16+15-15-15-15
+        // =3
+        console.log(value2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
